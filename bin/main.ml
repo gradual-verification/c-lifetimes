784896f568d039d360527e1906fbabe3b62609bc
timothytmzhou <timothytmzhou@gmail.com>
2022-06-28 16:32:05 -0400

    get CIL working

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_endline ""
+open Cil
+module E = Errormsg 
+
+let main () = 
+  let f = Frontc.parse Sys.argv.(1) () in
+  let d_list = Pretty.d_list "\n" d_global in 
+  E.log "%a" (d_list) f.globals;
+  ()
+
+let _ = main ()
