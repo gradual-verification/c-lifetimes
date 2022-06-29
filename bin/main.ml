62f3c8a7509bba46c1f1ccf2ec1c5212a419bc4b
timothytmzhou <timothytmzhou@gmail.com>
2022-06-29 17:18:03 -0400

    minor cleanup

--- bin/main.ml
+++ bin/main.ml
@@ -3,8 +3,8 @@ module E = Errormsg
 
 let main () = 
   let f = Frontc.parse Sys.argv.(1) () in
-  let d_list = Pretty.d_list "\n" d_global in 
-  E.log "%a" (d_list) f.globals;
+  let d_globals = Pretty.d_list "\n" d_global in 
+  E.log "%a" (d_globals) f.globals;
   ()
 
 let _ = main ()
