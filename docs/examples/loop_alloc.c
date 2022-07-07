997278de51cc1e377531dd5a6cf447a94eca4d3d
Ian McCormack <icmccorm@andrew.cmu.edu>
2022-07-07 12:57:51 -0400

    Added example verification language for recursive allocation/deallocation.

--- docs/examples/loop_alloc.c
+++ docs/examples/loop_alloc.c
@@ -1,17 +1,45 @@
 #include <stdlib.h>
 
+/*
+predicate alloc_list(int[] elements, int index){
+    if(index == 0){
+        true
+    }else{
+        alloc(elements[index-1]) and alloc_list(index - 1)
+    }
+}
+*/
 
-// this function should be total (no memory effects)
-// since it frees all the memory it allocates
-static void loop_alloc(int n) {
+/*
+predicate free_list(int[] elements, int index){
+    if(index == 0){
+        true
+    }else{
+        free(elements[index-1]) and free_list(index - 1)
+    }
+}
+*/
+
+static void loop_alloc(int n) 
+// (a -> void) : total
+{
     int** ptrs = (int**) malloc(n * sizeof(int*));
-    for (int i = 0; i < n; i++) {
-        ptrs[i] = (int*) malloc(sizeof(int));
-    }
 
-    for (int i = 0; i < n; i++) {
-        free(ptrs[i]);
-    }
+    //@fold alloc_list(ptrs, 0);
+    for (int i = 0; i < n; i++) 
+    //@loop_invariant alloc_list(ptrs, index);
+    {
+        //unfold alloc_list(ptrs, i);
+        ptrs[i] = (int*) malloc(sizeof(int)); // + alloc(ptrs[i])
+        //fold alloc_list(ptrs, i+1);
+    }  
 
-    free(ptrs);
-} 
+    //@fold free_list(ptrs, 0);
+    for (int i = 0; i < n; i++) 
+    //@loop_invariant free_list(ptrs, index)
+    {
+        //unfold free_list(ptrs, i);
+        free(ptrs[i]);                      // + free(ptrs[i])
+        //unfold free_list(ptrs, i+1);
+    }
+}
