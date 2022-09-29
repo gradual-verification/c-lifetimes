694b0c1aa9bfe9ff5eb44fb074558c6eabbf0338
Ian McCormack <icmccorm@andrew.cmu.edu>
2022-09-28 20:19:49 -0400

    Initial definition of abstract state and stub implementations for Cil.ForwardsDataFlow.

--- lib/abstract.ml
+++ lib/abstract.ml
@@ -1,9 +1,108 @@
+open Core
+open GoblintCil
+
 type liveness = Alive | Zombie | Dead
-let ( * ) (a:liveness) (b:liveness) =  match a with 
-  | Alive -> (match b with 
-  | Zombie -> Zombie | Dead -> Zombie | Alive -> Alive)
-  | Dead -> (match b with 
-      | Zombie -> Zombie | Dead -> Dead | Alive -> Zombie)
+
+let ( * ) (a : liveness) (b : liveness) =
+  match a with
+  | Alive -> (
+      match b with Zombie -> Zombie | Dead -> Zombie | Alive -> Alive)
+  | Dead -> ( match b with Zombie -> Zombie | Dead -> Dead | Alive -> Zombie)
   | Zombie -> Zombie
+  
+(** `
+  Both lifetime variables and abstract locations can be 
+  represented by a pair of ints, where the first value is 
+  the unique ID for a reference-type variable, and the second
+  is the level of indirection into its type.
+**)
+
+module AbstractValue = struct
+  module T = struct
+    type t = int * int [@@deriving sexp, compare]
+  end
+
+  include Comparable.Make (T)
+
+  let copy_set s = List.fold_left (Set.to_list s) ~init:Set.empty ~f:Set.add
+
+  let copy_map map copy =
+    List.fold_left (Map.keys map) ~init:Map.empty ~f:(fun m k ->
+        Map.add_exn m ~key:k ~data:(copy (Map.find_exn m k)))
+end
+
+module type AbstractMap = sig
+  type value
+  type t = value AbstractValue.Map.t
+
+  val join : t -> t -> t
+  val copy : t -> t
+  val initial : Cil.fundec -> t
+end
 
 
+module Sigma : AbstractMap = struct
+  type value = AbstractValue.Set.t
+  type t = value AbstractValue.Map.t
+
+  let join (a : t) (b : t) =
+    AbstractValue.Map.merge_skewed a b ~combine:(fun ~key:_ av bv ->
+        AbstractValue.Set.union av bv)
+
+  let copy c = AbstractValue.copy_map c AbstractValue.copy_set
+  let initial _fd = AbstractValue.Map.empty
+end
+
+module Chi : AbstractMap = struct
+  type value = liveness
+  type t = value AbstractValue.Map.t
+
+  let join (a : t) (b : t) =
+    AbstractValue.Map.merge_skewed a b ~combine:(fun ~key:_ av bv -> av * bv)
+
+  let copy c = AbstractValue.copy_map c Fn.id
+  let initial _fd = AbstractValue.Map.empty
+end
+
+module Phi : AbstractMap = struct
+  type value = Int.Set.t
+  type t = value AbstractValue.Map.t
+
+  let join (a : t) (b : t) =
+    AbstractValue.Map.merge_skewed a b ~combine:(fun ~key:_ av bv ->
+        Int.Set.union av bv)
+
+  let copy c =
+    AbstractValue.copy_map c (fun s ->
+        List.fold_left (Int.Set.to_list s) ~init:Int.Set.empty ~f:(fun s v ->
+            Int.Set.add s v))
+
+  let initial _fd = AbstractValue.Map.empty
+end
+
+module AbstractState = struct
+  type t = {
+    liveness: Chi.t;
+    mayptsto: Sigma.t;
+    reassignment: Phi.t;
+  }
+
+  let initial fd = {
+    liveness = Chi.initial fd;
+    mayptsto = Sigma.initial fd;
+    reassignment = Phi.initial fd;
+  }
+
+  let copy t = {
+    liveness = Chi.copy t.liveness;
+    mayptsto = Sigma.copy t.mayptsto;
+    reassignment = Phi.copy t.reassignment;
+  }
+
+  let join t1 t2 = {
+    liveness = Chi.join t1.liveness t2.liveness;
+    mayptsto = Sigma.join t1.mayptsto t2.mayptsto;
+    reassignment = Phi.join t1.reassignment t2.reassignment;
+  }
+
+end
---
+++ lib/transfer.ml
@@ -0,0 +1,54 @@
+open GoblintCil
+open Abstract
+module DF = Dataflow
+
+module LifetimeInferenceDefn = struct
+
+  type t = AbstractState.t
+  
+  let name = "Lifetime Inference"
+  let debug = ref false
+  let copy = AbstractState.copy
+  
+  let stmtStartData = Inthash.create 64
+  (** For each statement id, the data at the start. Not found in the hash
+   table means nothing is known about the state at this point. At the end
+    of the analysis this means that the block is not reachable. *)
+
+  (** Give the first value for a predecessors, compute the value to be set for the block *)
+  let computeFirstPredecessor _stmt state = state
+
+  (** Take some old data for the start of a statement, and some new data for
+    the same point. Return None if the combination is identical to the old
+    data. Otherwise, compute the combination, and return it. *)
+  let combinePredecessors _stmt ~old new_state =
+    Some (AbstractState.join old new_state)
+
+  (** Whether to put this statement in the worklist. This is called when a
+    block would normally be put in the worklist. *)
+  let filterStmt _stmt = true
+
+  let pretty _unit _s = Pretty.text "N/A"
+
+  let doInstr _instr state = DF.Done state
+  (** The (forwards) transfer function for an instruction. The
+     {!Cil.currentLoc} is set before calling this. The default action is to
+     continue with the state unchanged. *)
+
+  let doStmt _stmt _state = DF.SDone
+  (** The (forwards) transfer function for a statement. The {!Cil.currentLoc}
+     is set before calling this. The default action is to do the instructions
+     in this statement, if applicable, and continue with the successors. *)
+
+
+  let doGuard _exp _state = DF.GDefault
+    (** Generate the successor to an If statement assuming the given expression
+      is nonzero.  Analyses that don't need guard information can return
+      GDefault; this is equivalent to returning GUse of the input.
+      A return value of GUnreachable indicates that this half of the branch
+      will not be taken and should not be explored.  This will be called
+      twice per If, once for "then" and once for "else".
+    *)
+end
+
+module LifetimeInference = DF.ForwardsDataFlow(LifetimeInferenceDefn)
